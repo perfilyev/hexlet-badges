4ebd47b64c90a670ca916b40ac6d42482cf14585
Igor Perfilyev <perfilyevhead@gmail.com>
2016-08-03 19:40:34 +0000

    redirect landing to github

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ const UserCountRate = require('./user-count-rate');
 const UserCountScore = require('./user-count-score');
 const Badge = require('./badge');
 
-const server = http.createServer(({ url },res) => {
-  res.setHeader('Content-Type', 'image/png');
-  
-  https
-    .get(`https://ru.hexlet.io/u${url}`, r => 
-      r.pipe(getUserCount(url)).pipe(new Badge()).pipe(res))
+const server = http.createServer(({ url }, res) => {
+  if (url === '/') {
+    res.writeHead(302, {'Location': 'https://github.com/perfilyev/hexlet-badges'});
+    res.end();
+  } else {
+    res.setHeader('Content-Type', 'image/png');
+    
+    https.get(`https://ru.hexlet.io/u${url}`, userPage => 
+      userPage
+        .pipe(getUserCount(url))
+        .pipe(new Badge())
+        .pipe(res))
+  }
 }).listen(process.env.port || 8080);
 
 const getUserCount = url => {
